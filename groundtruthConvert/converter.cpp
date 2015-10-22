3f03b2b7b00051125fb4d356d11eebd213e72cbf
Hauke Mönck <moenck@zedat.fu-berlin.de>
2015-10-22 12:56:55 +0200

    Fixed csv to match the pipeline's ones.
    Signed-off-by: Hauke Mönck <moenck@zedat.fu-berlin.de>

--- groundtruthConvert/converter.cpp
+++ groundtruthConvert/converter.cpp
@@ -30,6 +30,19 @@
 
 #include <sstream>
 
+std::string fmt(const std::shared_ptr<Grid3D> grid3d, int index){
+	std::stringstream ss;
+	ss <<index << ",0,0,"
+			<< grid3d->getCenter().x << "," << grid3d->getCenter().y  << ","
+			<< grid3d->getXRotation() << ","<<  grid3d->getYRotation() << ","
+			<< grid3d->getZRotation() <<","<< grid3d->getPixelRadius()<< ",0,0,";
+
+
+	const std::array<boost::logic::tribool, 12ul> arr = grid3d->getIdArray();
+	for (unsigned int i=0; i<12; i++) ss << arr[i];
+	ss<<std::endl;
+	return ss.str();
+}
 
 void gtConverter::gtWorker::printTDatFile(std::string file){
 	Serialization::Data data;
@@ -41,6 +54,7 @@ void gtConverter::gtWorker::printTDatFile(std::string file){
 		ar(data);
 	}
 
+	int idx = 0;
 	GroundTruthEvaluation::ResultsByFrame resultsByFrame;
 	for (TrackedObject const& object : data.getTrackedObjects()) {
 		for (size_t frameNumber = 0; frameNumber <= object.getLastFrameNumber(); ++frameNumber) {
@@ -49,10 +63,9 @@ void gtConverter::gtWorker::printTDatFile(std::string file){
 			const std::shared_ptr<Grid3D> grid3d = object.maybeGet<Grid3D>(frameNumber);
 
 			if (!grid3d) continue;
-			std::cout <<
-					grid3d->getCenter().x << "," << grid3d->getCenter().y << ","<< grid3d->getPixelRadius() << ","<<
-					grid3d->getZRotation() << ","<<  grid3d->getYRotation() << ","<<
-					grid3d->getXRotation()<<std::endl;
+			std::string ss = fmt(grid3d,idx++);
+			std::cout << ss;
+
 		}
 	}
 
@@ -71,6 +84,7 @@ std::string gtConverter::gtWorker::TDatToCSV(std::string file){
 
 	std::stringstream ss;
 
+	int idx = 0;
 	GroundTruthEvaluation::ResultsByFrame resultsByFrame;
 	for (TrackedObject const& object : data.getTrackedObjects()) {
 		for (size_t frameNumber = 0; frameNumber <= object.getLastFrameNumber(); ++frameNumber) {
@@ -79,10 +93,8 @@ std::string gtConverter::gtWorker::TDatToCSV(std::string file){
 			const std::shared_ptr<Grid3D> grid3d = object.maybeGet<Grid3D>(frameNumber);
 
 			if (!grid3d) continue;
-			ss <<
-					grid3d->getCenter().x << "," << grid3d->getCenter().y << ","<< grid3d->getPixelRadius() << ","<<
-					grid3d->getZRotation() << ","<<  grid3d->getYRotation() << ","<<
-					grid3d->getXRotation()<<std::endl;
+			std::string r = fmt(grid3d,idx++);
+			ss << r;
 		}
 	}
 	return ss.str();
