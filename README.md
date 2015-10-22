539233752233ad4a900cf23e59102464e96100b9
Hauke Mönck <moenck@zedat.fu-berlin.de>
2015-10-22 11:51:53 +0200

    CLI done. Basic features of lib done.
    Signed-off-by: Hauke Mönck <moenck@zedat.fu-berlin.de>

---
+++ README.md
@@ -0,0 +1,34 @@
+groundtruthConvert README
+=========================
+
+groundtruthConvert is to tool to convert between csv and different ground truth data
+formats of the biorobotics lab. 
+
+## Implemented features
+
+* Print *.tdat as csv
+* Convert *.tdat to csv 
+
+## Building and usage
+
+* 	Requires alls dependencies of biorobotics_core and pipeline
+*	Requires boost program_options and opencv
+
+* 	use: 
+	cmake .
+	make
+	to build the library.
+
+* 	use:
+	cmake cmake -DBUILD_CLI=1 .
+	make
+	to build the command line tool.
+
+* 	For groundtruthConvertCLI, try 
+	groundtruthConvertCLI --help
+	to see the help text for conversion details.
+
+## Todo
+
+* Conversion between gt formats
+* csv -> tdat
--- groundtruthConvert/converter.cpp
+++ groundtruthConvert/converter.cpp
@@ -7,15 +7,8 @@
 
 #include "converter.h"
 
-#include <time.h>
-#include <cmath>        // std::abs
-
-
 #include <stdlib.h>
 #include <stdio.h>
-#include <sched.h>
-#include <chrono>
-#include <ctime>
 
 #include <pipeline/Decoder.h>
 #include <pipeline/EllipseFitter.h>
@@ -35,8 +28,10 @@
 #include <pipeline/util/GroundTruthEvaluator.h>
 #include "Grid3D.h"
 
+#include <sstream>
 
-void gtConverter::gtWorker::printFile(std::string file){
+
+void gtConverter::gtWorker::printTDatFile(std::string file){
 	Serialization::Data data;
 	{
 		std::ifstream is(file.c_str());
@@ -45,9 +40,6 @@ void gtConverter::gtWorker::printFile(std::string file){
 		// load serialized data into member .data
 		ar(data);
 	}
-
-	//pipeline::Tag t(cv::Rect(0,0,0,0),0);
-	//t.setValid(1);
 
 	GroundTruthEvaluation::ResultsByFrame resultsByFrame;
 	for (TrackedObject const& object : data.getTrackedObjects()) {
@@ -58,10 +50,43 @@ void gtConverter::gtWorker::printFile(std::string file){
 
 			if (!grid3d) continue;
 			std::cout <<
-					grid3d->getCenter().x << " | " << grid3d->getCenter().y << " | "<< grid3d->getPixelRadius() << " | "<<
-					grid3d->getZRotation() << " | "<<  grid3d->getYRotation() << " | "<<
+					grid3d->getCenter().x << "," << grid3d->getCenter().y << ","<< grid3d->getPixelRadius() << ","<<
+					grid3d->getZRotation() << ","<<  grid3d->getYRotation() << ","<<
 					grid3d->getXRotation()<<std::endl;
 		}
 	}
 
 }
+
+
+std::string gtConverter::gtWorker::TDatToCSV(std::string file){
+	Serialization::Data data;
+	{
+		std::ifstream is(file.c_str());
+		cereal::JSONInputArchive ar(is);
+
+		// load serialized data into member .data
+		ar(data);
+	}
+
+	std::stringstream ss;
+
+	GroundTruthEvaluation::ResultsByFrame resultsByFrame;
+	for (TrackedObject const& object : data.getTrackedObjects()) {
+		for (size_t frameNumber = 0; frameNumber <= object.getLastFrameNumber(); ++frameNumber) {
+			std::vector<GroundTruthGridSPtr>& results = resultsByFrame[frameNumber];
+
+			const std::shared_ptr<Grid3D> grid3d = object.maybeGet<Grid3D>(frameNumber);
+
+			if (!grid3d) continue;
+			ss <<
+					grid3d->getCenter().x << "," << grid3d->getCenter().y << ","<< grid3d->getPixelRadius() << ","<<
+					grid3d->getZRotation() << ","<<  grid3d->getYRotation() << ","<<
+					grid3d->getXRotation()<<std::endl;
+		}
+	}
+	return ss.str();
+
+}
+
+
